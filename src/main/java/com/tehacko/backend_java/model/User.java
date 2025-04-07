84bdbcea1f41447d9addc86ac1b3c7115051dcfd
Tom <cornelius.lundi@gmail.com>
2025-04-07 18:56:15 +0200

    another commit

--- src/main/java/com/tehacko/backend_java/model/User.java
+++ src/main/java/com/tehacko/backend_java/model/User.java
@@ -24,7 +24,40 @@ public class User {
     @Column(name = "is_admin", nullable = false) // Ensures correct DB column name
     private boolean isAdmin;
 
+    public int getUId() {
+        return uId;
+    }
+
+    public void setuId(int uId) {
+        this.uId = uId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
 }
+
 
 //    @Column(name = "birth_date") // Maps to PostgreSQL DATE column
 //    private LocalDate date;
