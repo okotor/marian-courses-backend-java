cbd99a55d38d8d6ece32d87f47133d99f7e3465e
Tom <cornelius.lundi@gmail.com>
2025-04-07 16:03:52 +0200

    14th push

--- src/main/java/com/tehacko/backend_java/BackendJavaApplication.java
+++ src/main/java/com/tehacko/backend_java/BackendJavaApplication.java
@@ -1,24 +1,11 @@
 package com.tehacko.backend_java;
 
-import com.tehacko.backend_java.factory.UserFactory;
-import com.tehacko.backend_java.model.User;
-import com.tehacko.backend_java.repo.UserRepo;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BackendJavaApplication {
     public static void main(String[] args) {
-        // Directly access environment variables
-        System.setProperty("GOOGLE_CLIENT_ID", System.getenv("GOOGLE_CLIENT_ID"));
-        System.setProperty("GOOGLE_CLIENT_SECRET", System.getenv("GOOGLE_CLIENT_SECRET"));
-        System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
-
         SpringApplication.run(BackendJavaApplication.class, args);
-
-	}
-
+    }
 }
